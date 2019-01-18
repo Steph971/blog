a16101b198ecc1962ab2f92944827d23e7987475
Stephen <stephen.germain@free.fr>
2019-01-18 23:29:17 +0100

    Create connected.php

---
+++ view/connected.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon blog</title>
+        <link href="style.css" rel="stylesheet" /> 
+    </head>
+        
+    <body>
+
+
+		<?php
+		if(isset($_SESSION['id']) AND isset($_SESSION['pseudo'])) {
+			echo 'bonjour' . $_SESSION['pseudo'];
+		}
+		?>
+
+		
+    </body>
+</html>
