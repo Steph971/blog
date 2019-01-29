312f105beb727095977ad6d82a0d2abcef877916
Stephen <stephen.germain@free.fr>
2019-01-30 00:09:09 +0100

    creation selectPost.php
    creation du fichier pour afficher le formulaire de modification

---
+++ view/selectPost.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon blog</title>
+        <link href="style.css" rel="stylesheet" /> 
+    </head>
+        
+    <body>
+        
+        <h1>
+		<?php
+
+		if(isset($_SESSION['pseudo'])) {
+			echo 'Bonjour ' .  $_SESSION['pseudo'];
+		}
+		?>
+    </h1>
+
+        <h2>Modifier l'article:</h2>
+
+        <form method="POST" action="index.php?page=updatePost">
+           <p> <label for="title">Titre: </label>
+            <input type="text" name="title" value="<?=$article['title'] ?>"></p>
+
+            <label for="content">Contenu:</label>
+            <textarea name="content" value="<?=$article['content'] ?>"></textarea>
+            <input type="submit">
+
+        </form>
+
+		
+    </body>
+</html>
