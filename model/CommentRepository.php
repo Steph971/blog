f02c1271dd783e071cf6dc93308a3704b98f1be1
Stephen <stephen.germain@free.fr>
2019-02-11 21:57:39 +0100

    maj
    ajout de la fonction getCommentByArticle
    
    ajout de la requete de selection des commentaires par article avec jointure des tables

--- model/CommentRepository.php
+++ model/CommentRepository.php
@@ -4,35 +4,48 @@ require('Comment.php');
 
 class CommentRepository extends Connect {
 	
-	function getComment()
+	function getComments()
 	{
 		$db = $this->getDb();
 
 		$req = $db->prepare('SELECT * FROM comments');
 		$req->execute();
 		
-		$comments = [];
+		$comment = [];
 		
 		while($data = $req-> fetch()) {
 			
-			$comments[] = $data;
+			$comment[] = $data;
 			
 		}
 		
 		$req->closeCursor();
 
-		return $comments;
+		return $comment;
 	}
 
 
+	function getCommentsByArticle()
+	{
+		$db = $this->getDb();
+
+		$req = $db->prepare('SELECT * FROM `comments` INNER JOIN user ON id_user=user.id WHERE id_post=:id_post');
+		$req->bindParam(':id_post', $_GET['id_post'], \PDO::PARAM_INT);
+		$req = $req->execute();
+
+		$comments = $req->fetch();
+
+		return $comments[0];
+	}
 	
 		
 	function addComment()
 	{
 		$db = $this->getDb();
 
-		$req = $db->prepare('INSERT INTO comments(name, message, date_mess) VALUES(:name, :message, NOW())');
-		$req->bindParam(':name', $_SESSION['pseudo'], \PDO::PARAM_STR);
+		$req = $db->prepare('INSERT INTO comments(id_user, id_post, message, date_mess) VALUES(:id_user, :id_post, :message, NOW())');
+		$req->bindParam(':id_user', $_SESSION['id'], \PDO::PARAM_INT);
+		$req->bindParam(':id_post', $_SESSION['post'], \PDO::PARAM_INT);
 		$req->bindParam(':message', $_SESSION['message'], \PDO::PARAM_STR);
 		$req->execute();
 		
