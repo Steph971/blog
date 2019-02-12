3612304493fa5d2dc411fe707cfc44817a971594
Stephen <stephen.germain@free.fr>
2019-02-12 22:51:50 +0100

    maj CommentRepository
    rajout de la requet SELECT dans la fonction getCommentsByArticle() avec jointure ainsi que la boucle while
    
    modification des valeurs (idUser et id_post) dans les $_SESSION de la fonction addComment

--- model/CommentRepository.php
+++ model/CommentRepository.php
@@ -28,24 +28,32 @@ class CommentRepository extends Connect {
 	function getCommentsByArticle()
 	{
 		$db = $this->getDb();
+		
+		$req = $db->prepare('SELECT * FROM comments INNER JOIN user ON id_user=user.id WHERE id_post=:id_post');
+		$req->bindParam(':id_post', $_SESSION['id_post'], \PDO::PARAM_INT);
+		$req->execute();
 
-		$req = $db->prepare('SELECT * FROM `comments` INNER JOIN user ON id_user=user.id WHERE id_post=:id_post');
-		$req->bindParam(':id_post', $_GET['id_post'], \PDO::PARAM_INT);
-		$req = $req->execute();
+		$comment = [];
+		
+		while($data = $req-> fetch()) {
+			
+			$comment[] = $data;
+			
+		}
 
-		$comments = $req->fetch();
+		$req->closeCursor();
 
-		return $comments[0];
+		return $comment;
 	}
 	
 		
 	function addComment()
 	{
 		$db = $this->getDb();
-
+		
 		$req = $db->prepare('INSERT INTO comments(id_user, id_post, message, date_mess) VALUES(:id_user, :id_post, :message, NOW())');
-		$req->bindParam(':id_user', $_SESSION['id'], \PDO::PARAM_INT);
-		$req->bindParam(':id_post', $_SESSION['post'], \PDO::PARAM_INT);
+		$req->bindParam(':id_user', $_SESSION['idUser'], \PDO::PARAM_INT);
+		$req->bindParam(':id_post', $_SESSION['id_post'], \PDO::PARAM_INT);
 		$req->bindParam(':message', $_SESSION['message'], \PDO::PARAM_STR);
 		$req->execute();
 		
