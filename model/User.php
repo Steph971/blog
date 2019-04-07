193cd0553c41eb17c8c0b5d06cf15816ade3c8cc
Stephen <stephen.germain@free.fr>
2019-04-07 22:17:46 +0200

    Maj User.php
    Ajout de l'attribut $id

--- model/User.php
+++ model/User.php
@@ -2,16 +2,22 @@
 
 class User {
 	
+	private $id;
 	private $username;
 	private $password;
 	
-	public function __construct($username, $password) {
+	public function __construct($id, $username, $password) {
 		
+		$this->id = $id;
 		$this->username = $username;
 		$this->password = $password;
 		
 	}
 	
+	public function getId() {
+		return $this->id;
+	}
+
 	public function getUsername() { // get pour recuperer la valeur d'une variable
 		return $this->username;
 	}
@@ -20,6 +26,12 @@ class User {
 		return $this->password;
 	}
 	
+	public function setId($id) {
+		
+		$this->id = $id;
+		
+	}
+
 	public function setUsername($username) { // set pour affecter une valeur a l'attribut
 		
 		$this->username = $username;
