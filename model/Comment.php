495286419c4588233aa966fcce5eceefae0eb228
Stephen <stephen.germain@free.fr>
2019-04-07 22:11:16 +0200

    Maj comment.php
    Ajout des attributs $id et $pseudo

--- model/Comment.php
+++ model/Comment.php
@@ -1,24 +1,53 @@
 <?php
 
-class Commnent {
+class Comment {
 	
+	private $id;
+	private $pseudo;
 	private $message;
+	private $date_mess;
 	
-	public function __construct($message) {
+	public function __construct($id, $pseudo,$message, $date_mess) {
 		
+		$this->id = $id;
+		$this->pseudo = $pseudo;
 		$this->message = $message;
+		$this->date_mess = $date_mess;
 		
 	}
 	
 	
+	public function getId() {
+		return $this->id;
+	}
+
+	public function getPseudo() {
+		return $this->pseudo;
+	}
+
 	public function getMessage() {
 		return $this->message;
 	}
+
+	public function getDate_mess() {
+		return $this->date_mess;
+	}
 	
-	
+	public function setId($id) {
+		
+		$this->id = $id;
+		
+	}
+
 	public function setMessage($message) {
 		
 		$this->message = $message;
+		
+	}
+
+	public function setDate_mess($date_mess) {
+		
+		$this->date_mess = $date_mess;
 		
 	}
 	
