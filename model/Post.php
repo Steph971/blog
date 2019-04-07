0b1dd41e68cea1397d63654ade49ad8ee62448b6
Stephen <stephen.germain@free.fr>
2019-04-07 22:17:14 +0200

    Maj Post.php
    Ajout des attributs $id, $author et $date_cont

--- model/Post.php
+++ model/Post.php
@@ -2,22 +2,46 @@
 
 class Post {
 	
+	private $id;
 	private $title;
 	private $content;
+	private $author;
+	private $date_cont;
 	
-	public function __construct($title, $content) {
+	public function __construct($id, $title, $content, $author, $date_cont) {
 		
+		$this->id = $id;
 		$this->title = $title;
 		$this->content = $content;
+		$this->author = $author;
+		$this->date_cont = $date_cont;
 		
 	}
 	
+	public function getId() {
+		return $this->id;
+	}
+
 	public function getTitle() {
 		return $this->title;
 	}
 	
 	public function getContent() {
 		return $this->content;
+	}
+
+	public function getAuthor() {
+		return $this->author;
+	}
+
+	public function getDate_cont() {
+		return $this->date_cont;
+	}
+
+	public function setId($id) {
+		
+		$this->id = $id;
+		
 	}
 	
 	public function setTitle($title) {
@@ -29,6 +53,18 @@ class Post {
 	public function setContent($content) {
 		
 		$this->content = $content;
+		
+	}
+
+	public function setAuthor($author) {
+		
+		$this->title = $author;
+		
+	}
+
+	public function setDate_cont($date_cont) {
+		
+		$this->title = $date_cont;
 		
 	}
 	
