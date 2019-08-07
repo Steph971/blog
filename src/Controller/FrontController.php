fda2aa318884e3970f6362efddbb30fcb9935020
Stephen <stephen.germain@free.Fr>
2019-08-07 14:21:45 +0200

    Update FrontController
    Added setSession method to modify superglobals

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-require('../vendor/autoload.php');
+//require('../vendor/autoload.php');
 
 use Twig\Environment;
 use Twig\Extension\DebugExtension;
@@ -9,13 +9,18 @@ use Twig\Loader\FilesystemLoader;
 
 class FrontController extends Controller {
 
+    private function setSession()
+    {
+        $_SESSION = $this->session;
+    }
+
 	public function run(){
 
 			
 		
-		if (isset($_GET['page']) && !empty($_GET['page'])) {
+		if (isset($this->get['page']) && !empty($this->get['page'])) {
 	
-			$page = $_GET['page'];
+			$page = $this->get['page'];
 	
 		} else {
 	
@@ -25,45 +30,50 @@ class FrontController extends Controller {
 
 		switch ($page)	{
 
+
 			case "home" :
 				$postController = new PostController();
 				$postController->listPosts();
 				break;
 	
 			case "listUsers" : 
-				$userController = new UserController($twig);
+				$userController = new UserController();
 				$userController->listAllUsers();
 				break;
 
 	
 			case "addUser" :
-				$_SESSION['pseudo'] = $_POST['pseudo'];
-				$_SESSION['password'] = $_POST['password'];	
-				$userController = new UserController($twig);
+				$this->session['pseudo'] = $this->post['pseudo'];
+				$this->session['password'] = $this->post['password'];
+				$userController = new UserController();
+				$this->setSession();
 				$userController->showAddUser();
 				break;
 	
 			case "subscribe" : 
-				$userController = new UserController($twig);
+				$userController = new UserController();
 				$userController->subscribe();
 				break;
 
 			case "delete" :					
-				$_SESSION['id'] = $_GET['id'];											
-				$userController = new UserController($twig);		
+				$this->session['id'] = $this->get['id'];
+				$userController = new UserController();
+				$this->setSession();
 				$userController->showDeleteUser();	
 				break;		
 
 			case "editUser" : 
-				$_SESSION['id'] = $_GET['id'];
-				$userController = new UserController($twig);
+				$this->session['id'] = $this->get['id'];
+				$userController = new UserController();
+				$this->setSession();
 				$userController->editUser();
 				break;		
 
 			case "updateUser" :
-				$_SESSION['pseudo'] = $_POST['pseudo'];
-				$_SESSION['password'] = $_POST['password'];	
-				$userController = new UserController($twig);
+				$this->session['pseudo'] = $this->post['pseudo'];
+				$this->session['password'] = $this->post['password'];
+				$userController = new UserController();
+				$this->setSession();
 				$userController->showUpdateUser();
 				break;
 
@@ -76,20 +86,21 @@ class FrontController extends Controller {
 				$this->session['pseudo'] = $this->post['pseudo'];
 				$this->session['password'] = $this->post['password'];
 				$userController = new UserController();
+                $this->setSession();
 				$userController->showConnectAdmin();
 				$userController->connected();	
 				break;
 
 			case "addArticles" :						
-				$userController = new UserController($twig);
+				$userController = new UserController();
 				$userController->showConnectAdmin();
 				$userController->addArticles();	
 				break;
 
 			case "moderation" :
-				if(isset($_SESSION['pseudo']) && isset($_SESSION['password']) && isset($_SESSION['level'])) {
-					if($_SESSION['level'] == "2" ){
-						$commentController = new CommentController($twig);
+				if(isset($this->session['pseudo']) && isset($this->session['password']) && isset($this->session['level'])) {
+					if($this->session['level'] == "2" ){
+						$commentController = new CommentController();
 						$commentController->showCommentsValid();
 					}
 		 			else {
@@ -102,38 +113,42 @@ class FrontController extends Controller {
 				break;
 
 			case "listeArticles" : 
-				$postController = new PostController($twig);
+				$postController = new PostController();
 				$postController->showAllPosts();
 				break;
 
 			case "addPost" : 
-				$_SESSION['title'] = $_POST['title'];
-				$_SESSION['content'] = $_POST['content'];
-				$postController = new PostController($twig);
+				$this->session['title'] = $this->post['title'];
+				$this->session['content'] = $this->post['content'];
+				$postController = new PostController();
+				$this->setSession();
 				$postController->showAddPost();
 				break;	
 
 			case "selectPost" : 
-				$_SESSION['id'] = $_GET['id'];
-				$postController = new PostController($twig);
+				$this->session['id'] = $this->get['id'];
+				$postController = new PostController();
+				$this->setSession();
 				$postController->showSelectPost();
 				break;
 
 			case "updatePost" : 
-				$_SESSION['title'] = $_POST['title'];
-				$_SESSION['content'] = $_POST['content'];
-				$postController = new PostController($twig);
+				$this->session['title'] = $this->post['title'];
+				$this->session['content'] = $this->post['content'];
+				$postController = new PostController();
+				$this->setSession();
 				$postController->showUpdatePost();
 				break;
 
 			case "deletePost" : 
-				$_SESSION['id'] = $_GET['id'];
-				$postController = new PostController($twig);
+				$this->session['id'] = $this->get['id'];
+				$postController = new PostController();
+				$this->setSession();
 				$postController->showDeletePost();
 				break;
 
 			case "deconnexion" :
-				$userController = new UserController($twig);
+				$userController = new UserController();
 				$userController->deconnexion();
 				break;
 
@@ -144,39 +159,45 @@ class FrontController extends Controller {
 				//break;
 
 			case "addComment" : 
-				$_SESSION['message'] = $_POST['message'];
-				$commentController = new CommentController($twig);
+				$this->session['message'] = $this->post['message'];
+				$commentController = new CommentController();
+				$this->setSession();
 				$commentController->showAddComment();
-				$postController = new PostController($twig);
+				$postController = new PostController();
+				$this->setSession();
 				$postController->showPost();	
 				break;
 
 			case "getPost" : 
-				$_SESSION['id_post'] = $_GET['id'];
+				$this->session['id_post'] = $this->get['id'];
 				$postController = new PostController();
+				$this->setSession();
 				$postController->showPost();
 				break;
 
 			case "getCommentsValid" :
-				$commentController = new CommentController($twig);
+				$commentController = new CommentController();
 				$commentController->showCommentsValid();
 				break;
 
 			case "validComment" : 
-				$_SESSION['id'] = $_GET['id'];
-				$commentController = new CommentController($twig);
+				$this->session['id'] = $this->get['id'];
+				$commentController = new CommentController();
+				$this->setSession();
 				$commentController->showValidComment();
 				break;
 
 			case "suppComment" : 
-				$_SESSION['id'] = $_GET['id'];
-				$commentController = new CommentController($twig);
+				$this->session['id'] = $this->get['id'];
+				$commentController = new CommentController();
+				$this->setSession();
 				$commentController->showSuppComment();
 				break;	
 
 			case "flagComment" : 
-				$_SESSION['id'] = $_GET['id'];
-				$commentController = new CommentController($twig);
+				$this->session['id'] = $this->get['id'];
+				$commentController = new CommentController();
+				$this->setSession();
 				$commentController->showFlagComment();
 				break;
 		}
