69a57ba03bee87d2cb9cfd3bfb02fd3bb9f1abdd
Stephen <stephen.germain@free.Fr>
2019-11-01 10:55:36 +0100

    Print_r
    To perform tests

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 $frontController = new FrontController();
-// print_r($_SESSION);
+//print_r($_POST);
+//print_r($_SESSION);
 // var_dump($frontController);
 $frontController->run();
